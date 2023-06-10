9704ff7ca14ace6f0e824db6cd5e4797a8fa138e
kusunoki576 <84463281+kusunoki576@users.noreply.github.com>
2023-06-10 14:46:23 +0900

    Add files via upload

--- Personality-ChatBot.py
+++ Personality-ChatBot.py
@@ -31,8 +31,6 @@ system_input_basis_prompt = f"""
 *あなたは{age}歳の{gender}です。
 *あなたの名前は{name}です。
 *あなたの性格は{personality}です
-*あなたの趣味は{hobby}です
-*あなたは{hobby}に関して深い知識をもっています。
 """
 
 intelligence_text = utils.make_intelligence_text(intelligence_value)
@@ -55,11 +53,13 @@ system_input_prompt = f"""
 {NG_Words}
 """
 
+new_system = f"あなたはキャラクターになりきって会話をする人です。あなたの趣味は{hobby}です。あなたは{hobby}に関して深い知識をもっています。以下の制約条件を守りつつ、次の属性がある人になりきって会話してください。"
+
 system_input_basis = st.text_area("System Basis Prompt", key="system_basis_input", height=200, value=system_input_basis_prompt)
 st.write(f"知性(IQ)は{intelligence_value}, 社交性は{sociability_value}です。(知性と社交性に応じて下のプロンプトが変化します)")
 st.write(f"temperature={temperature}")
 system_input = st.text_area("System Prompt", key="system_input", height=200, value=system_input_prompt)
-st.write(f"システムには次の命令が与えられています:\n \"あなたはキャラクターになりきって会話をする人です。以下の制約条件を守りつつ、次の属性がある人になりきって会話してください。\"")
+st.write(f"システムには次の命令が与えられています:\n \"{new_system}\"")
 user_input = "以下の質問に答えてください\n" + st.text_input("質問", key="user_input")
 
 button = st.button("Submit")
@@ -67,7 +67,7 @@ if button or st.session_state.get("submit"):
     responce = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "あなたはキャラクターになりきって会話をする人です。以下の制約条件を守りつつ、次の属性がある人になりきって会話してください。"},
+            {"role": "system", "content": new_system},
             {"role": "user", "content": system_input_basis + system_input + user_input}
         ],
         temperature=temperature
